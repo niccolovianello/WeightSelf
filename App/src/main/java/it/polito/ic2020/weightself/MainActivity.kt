993d42fc83474adeacf8ccfcb0be8c86db6e892b
brugogaia <brugogaia@users.noreply.github.com>
2021-01-09 16:23:36 +0100

    aggiunto //ciao in MainActivity.kt

--- App/src/main/java/it/polito/ic2020/weightself/MainActivity.kt
+++ App/src/main/java/it/polito/ic2020/weightself/MainActivity.kt
@@ -2,12 +2,13 @@ package it.polito.ic2020.weightself
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.materialexamples.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //ciao amici
+        //ciao a te
     }
 }
